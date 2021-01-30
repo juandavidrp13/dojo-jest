44ffc6b70298002f991e9d3f8c8416f95def2c72
Juandavidrp <jesusisaza@gmail.com>
2021-01-30 08:56:35 -0500

    New configuration added to deploy on heroku

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require("./app")
+var port = process.env.PORT|| 3000; //esto lee el puerto de heroku y local el 3000
 
-app.listen(3000, ()=>{
+
+app.listen(port, ()=>{
     console.log("running")
 })
